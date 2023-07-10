de9ed925866f5f1885926403423ebbeb9ee4f070
Macbook Pro <macbookpro@Dins-MacBook-Pro.local>
2023-07-10 13:09:51 +0300

    First push

---
+++ BellaOlonjeMain/src/components/Phones/Phones.jsx
@@ -0,0 +1,21 @@
+import "./Phones.css";
+import Phone from "../../assets/Phones.png";
+import FadeInSection from "../FadeInSection";
+
+const Phones = () => {
+  return (
+    <>
+      <div className="phone_wrapper">
+        <img className="phone" src={Phone} alt="phone" />
+      </div>
+      <div className="headline_wrapper">
+        <div className="breaker"></div>
+        <FadeInSection>
+          <p className="phone__title">How the app works</p>
+        </FadeInSection>
+      </div>
+    </>
+  );
+};
+
+export default Phones;
---
+++ BellaOlonjeMain/src/layouts/ImageTextLayout/ImageTextLayout.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+import FadeInSection from "../../components/FadeInSection";
+import "./ImageTextLayout.css";
+
+const ImageTextLayout = ({
+  imageSrc,
+  text,
+  subtitle,
+  title,
+  isImageOnRight,
+}) => {
+  return (
+    <div
+      className={
+        isImageOnRight
+          ? "image-text-container right"
+          : "image-text-container left"
+      }
+    >
+      <div className="image-container">
+        <img src={imageSrc} alt="Image" />
+      </div>
+      <div className="text-container">
+        <FadeInSection>
+          <h1>{subtitle}</h1>
+        </FadeInSection>
+
+        <FadeInSection>
+          <h2>{title}</h2>
+        </FadeInSection>
+
+        <FadeInSection>
+          <h3>{text}</h3>
+        </FadeInSection>
+      </div>
+    </div>
+  );
+};
+
+ImageTextLayout.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  isImageOnRight: PropTypes.bool.isRequired,
+};
+
+export default ImageTextLayout;
